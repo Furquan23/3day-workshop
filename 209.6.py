b1dab24a52c482af302427e1826be0e5b33fd5c2
Furquan23 <157411550+Furquan23@users.noreply.github.com>
2026-09-08 11:09:29 +0530

    Add files via upload

---
+++ 209.6.py
@@ -0,0 +1,8 @@
+num=int(input("Enter any number"))
+
+if num>0:
+    print(f"{num} is positive integer")
+elif num<0:
+    print(f"{num} is negative integer")
+else:
+    print("the integer is zero")
