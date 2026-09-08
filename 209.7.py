4fd0a1d5296fa5c4650988d536beed246de149c3
Furquan23 <157411550+Furquan23@users.noreply.github.com>
2026-09-08 11:15:27 +0530

    Add files via upload

---
+++ 209.7.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+num3=int(input("Enter third number:"))
+
+if num1==num2 and num1==num3:
+    print("All three numbers are equal")
+else:
+    print("All three numbers are not equal")
