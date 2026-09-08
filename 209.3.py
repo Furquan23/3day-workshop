cbe1493ebf4781080f676672bf28c60202720110
Furquan23 <157411550+Furquan23@users.noreply.github.com>
2026-09-08 10:50:07 +0530

    Add files via upload

---
+++ 209.3.py
@@ -0,0 +1,7 @@
+
+Temp = float(input("Enter temperature in Celsius"))
+Fahrenheit = ((9/5) * Temp) + 32
+print(f"Temperature in fahrenheit is:{Fahrenheit}")
+
+Celsius = (Fahrenheit - 32) * 5/9
+print(f"temperature in Celsius is:{Celsius}")
